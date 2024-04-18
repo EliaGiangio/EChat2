e050ef473a84c49b35bd2077ebbd2d85ecaadc32
EliaGiangio <eliagiang@yahoo.com>
2024-04-18 14:34:48 +0200

    added dotenv

--- src/firebase.js
+++ src/firebase.js
@@ -6,14 +6,14 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDH58TwbpA7wvJdr40bcF-PnLgNsiPPHW4",
-    authDomain: "thesocialnetwork-43f03.firebaseapp.com",
-    databaseURL: "https://thesocialnetwork-43f03-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "thesocialnetwork-43f03",
-    storageBucket: "thesocialnetwork-43f03.appspot.com",
-    messagingSenderId: "512049929222",
-    appId: "1:512049929222:web:5070ec382431b51dc97787",
-    measurementId: "G-8QWKJ0QF60"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
