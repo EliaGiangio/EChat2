c1138db074b28e9c1d11f18ce665fc0036af80e6
EliaGiangio <eliagiang@yahoo.com>
2024-04-19 10:34:58 +0200

    prepared git config for ghpages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
-// import dotenv package
 import dotenv from 'dotenv';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
-// run package config
 dotenv.config();
-// https://vitejs.dev/config/
+
 export default defineConfig({
+  base: '/EChat2.0/',
   plugins: [react()],
-  // define process env
   define: {
     'process.env': process.env
   }
